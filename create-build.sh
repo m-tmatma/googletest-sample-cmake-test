136e8615e190968eb3d467e1cdc69b6b209f2d1e
Masaru Tsuchiyama <m.tmatma@gmail.com>
2017-12-28 20:59:59 +0900

    add build script

---
+++ create-build.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+if [ ! -e googletest ]; then
+    git submodule init
+    git submodule update
+fi
+
+DIR=build
+if [ "$(uname)" == 'Darwin' ]; then
+    CMAKE=/Applications/CMake.app/Contents/bin/cmake
+elif [ "$(expr substr $(uname -s) 1 5)" == 'Linux' ]; then
+    CMAKE=/usr/bin/cmake
+else
+    echo "Your platform ($(uname -a)) is not supported."
+    exit 1
+fi
+
+rm -rf $DIR
+
+mkdir $DIR
+$CMAKE -B$DIR -H. || exit 1
+make -C $DIR      || exit 1
+
